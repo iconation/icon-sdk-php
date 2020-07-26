8df26997158cb65cec4ea95ebfc0267dd922bf7e
Dimitris Frangiadakis <dimitris@frangiadakis.com>
2020-07-26 15:42:31 +0300

    Handles exceptions from RPC server

--- src/Utils/IconServiceHelper.php
+++ src/Utils/IconServiceHelper.php
@@ -15,13 +15,15 @@ class IconServiceHelper
 
     /**
      * @param $data
-     * @return object
+     * @return object|null
+     * @throws \Exception
      */
     public function sendRequest($data)
     {
         //Send request to RPC
         $data_string = json_encode($data);
         $ch = curl_init($this->iconService->getIconServiceUrl());
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -29,6 +31,15 @@ class IconServiceHelper
             'Content-Type: application/json'
         ));
 
-        return json_decode(curl_exec($ch));
+        $result = curl_exec($ch);
+
+        if (curl_errno($ch) !== 0 || strpos(strtolower($result), 'jsonrpc') === false) {
+            curl_close($ch);
+            throw new \Exception('Curl error' . curl_error($ch) !== '' ? curl_error($ch) : $result );
+        }
+
+        curl_close($ch);
+        return json_decode($result);
+
     }
 }
